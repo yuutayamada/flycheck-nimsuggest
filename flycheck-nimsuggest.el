482e6ca8c11dae36351fbdf851b9a4ab66a0c23f
Yuta Yamada <cokesboy@gmail.com>
2017-10-25 01:24:17 -0700

    Fix version info to specify cl-lib's version

--- flycheck-nimsuggest.el
+++ flycheck-nimsuggest.el
@@ -5,7 +5,7 @@
 ;; Author: Yuta Yamada <cokesboy"at"gmail.com>
 
 ;; Version: 0.8.1
-;; Package-Requires: ((flycheck "0.23") (nim-mode "0.4.1") (emacs "24"))
+;; Package-Requires: ((flycheck "0.23") (nim-mode "0.4.1") (emacs "24.3"))
 ;; URL: https://github.com/yuutayamada/flycheck-nimsuggest
 
 ;;; License:
